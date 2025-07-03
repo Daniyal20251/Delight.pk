d8e88575c912cfed7c3ad1f19ed852114678cba9
Daniyal20251 <araindaniyalo2@gmail.com>
2025-07-03 15:58:30 +0500

    Add files via upload

---
+++ itemData.js
@@ -0,0 +1,39 @@
+const items = [
+  {
+    title:  "Wireless Earbuds",
+    price: "Rs. 990",
+    description: `
+    <ul>
+  Material: ABS Plastic
+  Connectivity Tech: Bluetooth
+  Color: Multicolor
+  Package Includes: 1 x Earbuds<
+  Call Time: 4 Hours
+  Note: Please ensure to follow
+  instructions provided in the user manual
+  for proper usage and safety precautions.
+  Product Code: MZ53500000018HHK
+</ul>`,
+  image:"10.jpg",
+  images: [
+    "10.jpg",
+    "11.jpg",
+    "12.jpg",
+    "13.jpg",
+    "14.jpg",
+    "15.jpg"
+    ]
+  },
+  {
+    title: "Smart watch",
+    price: "Rs. 2000",
+    description: "No:1 Smart watch ultra 9",
+    image:"1.jpg",
+    images:[
+      "1.jpg",
+    "2.jpg",
+    "3.jpg",
+    "4.jpg"
+      ]
+  },
+];
