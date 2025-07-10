1847290c89a67bcfe34e19e0dad8d5c5efd870ed
Daniyal20251 <araindaniyalo2@gmail.com>
2025-07-10 16:57:23 +0500

    Add files via upload

--- itemData.js
+++ itemData.js
@@ -1,4 +1,314 @@
 const items = [
   {
-    
+  title: "Versatile Mobile Phone - 1 Pcs, 120 Watts Power",
+  price: "Rs.4480",
+  image: "1752140891907.jpg",
+  images: [
+    "1752140891907.jpg",
+    "1752140892480.jpg",
+    "1752140892717.jpg",
+    "1752140893025.jpg"
+  ],
+  colors: ["Blue", "Green", "White", "Golden", "Purple"],
+  description: `
+<ul>
+  <li><strong>Product Code: DP61600000006MMMEADAS</strong></li>
+  <li><strong>Product Description</strong>: Discover the future of communication with our Mobile Phone, boasting a powerful 120 Watts and a sleek, user-friendly design. This essential device comes in multiple vibrant colors and is perfect for anyone looking for a reliable mobile experience. With a battery capacity of 2000 and an impressive standby time of 36 hours, stay connected on the go!</li>
+  <li>*Product Details*:Material: Plastic</li>
+  <li>Color: Blue, Green, White, Golden, Purple</li>
+  <li>Package Includes: 1 x Mobile Phone</li>
+  <li>Battery Capacity: 2000 mAh</li>
+  <li>Standby Time: 36 Hours</li>
+  <li>Voltage: 20 A</li>
+  <li>Watts: 120 Watt</li>
+  <li>Note: There might be an error of 1-3 cm due to manual measurement, and slight color differences may occur as a result of varying lighting and monitor effects.</li>
+</ul>`},
+{
+  title: "Mini Nokia 106 Mobile",
+  price: "Rs.2810",
+  image: "1752141321761.jpg",
+  images: [
+    "1752141321761.jpg",
+    "1752141321118.jpg",
+    "1752141321385.jpg",
+    "1752141321984.jpg"
+  ],
+  colors: [],
+  description: `
+<ul>
+  <li><strong>Product Code: DP59700050MRMMA</strong></li>
+  <li><strong>Product Description</strong>: Mobile Phones For Everyday Use</li>
+  <li>*Product Details*:Material: Plastic</li>
+  <li>Product Features: Frequency: 2G band 900/1800, Sim Status: Dual sim, Battery Time :Li-Ion 800 mAh Removable, Stand-by Up To 619 h, Talk Time Up To 14 h 24 Min, Color: Black</li>
+  <li>Non PTA, No Warranty</li>
+  <li>Package Includes: 1 x Charger, Battery, Mobile</li>
+  <li>Product Dimensions (Inches, cm, m): 119 x 49.2 x 14.4 mm (4.69 x 1.94 x 0.57 Inches), Weight (pounds, Oz, Grams): 70.00 gram</li>
+  <li>Note: Please ensure to follow the instructions provided in the user manual for proper usage and safety precautions.</li>
+</ul>
+`
+},
+{
+  title: "i16 Earbuds",
+  price: "Rs.840",
+  image: "1752141705128.jpg",
+  images: [
+    "1752141705128.jpg",
+    "1752141705789.jpg",
+    "1752141706454.jpg",
+    "1752141706693.jpg",
+    "1752141707122.jpg"
+  ],
+  colors: [],
+  description: `
+<ul>
+  <li><strong>Product Code: DP63300000012GSHE</strong></li>
+  <li><strong>Product Description</strong>: </li>
+  <li>*Product Details*:Material: ABS Plastic</li>
+  <li>Connectivity Tech: Bluetooth</li>
+  <li>Bluetooth Version: Bluetooth 4</li>
+  <li>Product Feature: Long Battery Life, Water Resistant, Clear Voice, Compatible With All Android Devices, Compatible With iPhone Devices</li>
+  <li>Package Includes: 1 x Earbuds</li>
+  <li>16 Model Number</li>
+  <li>Battery Capacity: 600 mAh</li>
+  <li>Call Time: 3 Hours</li>
+  <li>Standby Time: 100 Hours</li>
+  <li>Voltage: 5 Volts</li>
+  <li>Weight: .2 Kgs</li>
+  <li>Length: 12 Cm</li>
+  <li>Width: 6 Cm</li>
+  <li>Height: 4 Cm</li>
+  <li>Pack Of 1</li>
+  <li>Color: White</li>
+  <li>Note: Please ensure to follow the instructions provided in the user manual for proper usage and safety precautions.</li>
+</ul>`},
+{
+  title: "*Product Name*: Air 31 Wireless Earbuds",
+  price: "Rs.900",
+  image: "1752142079559.jpg",
+  images: [
+    "1752142079559.jpg",
+    "1752142079842.jpg",
+    "1752142080920.jpg"
+  ],
+  colors: ["Blue", "Green", "White", "Blue", "Pink"],
+  description: `
+<ul>
+  <li><strong>Product Code: DP63320014GSHE</strong></li>
+  <li><strong>Product Description</strong>: </li>
+  <li>*Product Details*:Material: Plastic, Silicone, TP</li>
+  <li>Package Includes: 1 x Earbuds</li>
+  <li>Color: Blue, Green, White, Blue, Pink</li>
+  <li>Note: Please ensure to follow the instructions provided in the user manual for proper usage and safety precautions.</li>
+</ul>
+`},
+{
+  title:"Wireless Stereo Headphones",
+  price: "Rs.1040",
+  image: "1752145733704.jpg",
+  images: [
+    "1752145733704.jpg",
+    "1752145733959.jpg",
+    "1752145730052.jpg",
+    "1752145731089.jpg",
+    "1752145732084.jpg"
+  ],
+  colors: ["Black", "Blue", "White", "Green", "Red"],
+  description: `
+<ul>
+  <li><strong>Product Code: DP70420004MTAS</strong></li>
+  <li><strong>Product Description</strong>: </li>
+  <li>*Product Details*:Material: ABS Plastic</li>
+  <li>Waterproof (Yes/No): No</li>
+  <li>Color:Black ,Blue ,White ,Green ,Red</li>
+  <li>Operation Distance: 10m;</li>
+  <li>Support : Bluetooth, Aux in, TF Card, FM radio</li>
+  <li>Charging Time of Device: 1.5Hrs</li>
+  <li>Bluetooth Version: 4.2-5.0</li>
+  <li>Frequency: 2.4 GHz 2.4835 GHz</li>
+  <li>Battery Timing: 4-5Hrs</li>
+  <li>USB charging cable: AC input 110-240 V DC</li>
+  <li>Package Includes: 1 x Bluetooth Headphone, 1 x USB Cable</li>
+  <li>Note: Please ensure to follow the instructions provided in the user manual for proper usage and safety precautions.</li>
+</ul>
+`},
+{
+  title: "Stylish White Bluetooth 5 Earbuds with Long Battery Life",
+  price: "Rs.1250",
+  image: "1752146219248.jpg",
+  images: [
+    "1752146219248.jpg",
+    "1752146226811.jpg",
+    "1752146227739.jpg",
+    "1752146228481.jpg"
+  ],
+  colors: ["White"],
+  description: `
+<ul>
+  <li><strong>Product Code: DP57800000116GALAXY</strong></li>
+  <li><strong>Product Description</strong>: Experience the ultimate in wireless audio with our sleek and stylish white Bluetooth 5 earbuds. Designed for comfort and convenience, these earbuds offer long battery life and seamless connectivity across devices. Perfect for music lovers and on-the-go lifestyles, enjoy crystal clear sound and a modern design that fits your lifestyle.</li>
+  <li>*Product Details*:Material: Plastic</li>
+  <li>Connectivity Tech: Bluetooth</li>
+  <li>Bluetooth Version: Bluetooth 5</li>
+  <li>Product Feature: Long Battery Life, Sleek And Stylish Design</li>
+  <li>Color: White</li>
+  <li>Package Includes: 1 x Earbuds, 1 x Charging Case</li>
+  <li>Note: Please ensure to follow the instructions provided in the user manual for proper usage and safety precautions.</li>
+</ul>
+`},
+{
+  title: "Beautiful Mix Digit Wood Wall Clock",
+  price: "Rs.470",
+  image: "1752146607310.jpg",
+  images: [
+    "1752146607310.jpg",
+    "1752146608118.jpg",
+    "1752146612902.jpg"
+  ],
+  colors: [],
+  description: `
+<ul>
+  <li><strong>Product Code: DP24800165HDHDC</strong></li>
+  <li><strong>Product Description</strong>: Attractive Wall Clock Designs For Homes And Office</li>
+  <li>*Product Details*:Material: Chipboard Laminated Sheet</li>
+  <li>Product Type: Wall Clock</li>
+  <li>Size: 24 x 24 Inches</li>
+  <li>Thickness: 3mm</li>
+  <li>Finish Type: Laminated</li>
+  <li>Display: 12-Hour Display</li>
+  <li>Package Includes: 1 x Wall Clock</li>
+  <li>Product Details: Laser Cut, Complete Clock With Double Tape To Hang. Make Your Home Beautiful. This Product Enhances Any Home With On-trend Style And Beautiful Eye-catching Modern Design</li>
+  <li>Disclaimer: Product color may slightly vary due to photographic lighting sources or your monitor/screen settings.</li>
+</ul>
+`},
+{
+  title: "Butterfly Laminated Wall Clock With Backlight",
+  price: "Rs.620",
+  image: "1752146826716.jpg",
+  images: [
+    "1752146826716.jpg",
+    "1752146828215.jpg",
+    "1752146829165.jpg"
+  ],
+  colors: [],
+  description: `
+<ul>
+  <li><strong>Product Code: DP24800188HDHDC</strong></li>
+  <li><strong>Product Description</strong>: Attractive Wall Clock Designs For Homes And Office With Light</li>
+  <li>*Product Details*:Material: Chipboard Laminated Sheet</li>
+  <li>Product Type: Wall Clock With Light</li>
+  <li>Size: 24 x 24 Inches</li>
+  <li>Thickness: 3mm</li>
+  <li>Finish Type: Laminated</li>
+  <li>Display: 12-Hour Display</li>
+  <li>Package Includes: 1 x Wall Clock</li>
+  <li>Product Details: Laser Cut, Complete Clock With Double Tape To Hang. Make Your Home Beautiful. This Product Enhances Any Home With On-trend Style And Beautiful Eye-catching Modern Design</li>
+  <li>Disclaimer: Product color may slightly vary due to photographic lighting sources or your monitor/screen settings.</li>
+</ul>
+`},
+{
+  title: "Multicolor Smart Watch with Long Battery Life",
+  price: "Rs.1799",
+  image: "1752147190560.jpg",
+  images: [
+    "1752147190560.jpg",
+    "1752147191389.jpg",
+    "1752147188841.jpg",
+    "1752147190560.jpg",
+    "1752147190885.jpg"
+  ],
+  colors: ["Multicolor"],
+  description: `
+<ul>
+  <li><strong>Product Code: DP1172200277SE</strong></li>
+  <li><strong>Product Description</strong>: Discover the ultimate convenience with our Smart Watch featuring a vibrant multicolor design and long-lasting battery life. This innovative device blends functionality and style, making it perfect for tracking your daily activities and staying connected. Elevate your wrist game with a versatile accessory that fits seamlessly into your lifestyle.</li>
+  <li>*Product Details*:Material: Metal, Silicone</li>
+  <li>Product Feature: Long Battery Life</li>
+  <li>Color: Multicolor</li>
+  <li>Package Includes: 1 x Smart Watch</li>
+  <li>Note: Please ensure to follow the instructions provided in the user manual for proper usage and safety precautions.</li>
+</ul>
+`},
+{
+  title: "Stylish Smart Watch - 1 Pcs - Available in Black, Red & White",
+  price: "Rs.1410",
+  image: "1752147533184.jpg",
+  images: [
+    "1752147533184.jpg",
+    "1752147535236.jpg",
+    "1752147536426.jpg",
+    "1752147534151.jpg",
+    "1752147537146.jpg"
+  ],
+  colors: ["Black", "Red", "White"],
+  description: `
+<ul>
+  <li><strong>Product Code: DP1518200222BGSE</strong></li>
+  <li><strong>Product Description</strong>: Discover the perfect companion for your daily activities with this stylish Smart Watch. Designed from durable ABS plastic, it features a sleek interface and comes in three vibrant colors: Black, Red, and White. Enhance your lifestyle and stay connected effortlessly with this versatile timepiece, ideal for any occasion. Package includes 1 x Smart Watch, the ultimate accessory for tech-savvy individ</li>
+  <li>*Product Details*:Material: ABS Plastic</li>
+  <li>Color: Black, Red, White</li>
+  <li>Package Includes: 1 x Smart Watch</li>
+  <li>Note: Please ensure to follow the instructions provided in the user manual for proper usage and safety precautions.</li>
+</ul>
+`
+},
+{
+  title: "Ultra 2 - Smart Watch Four Strips & P47 Headphones",
+  price: "Rs.3120",
+  image: "1752147809896.jpg",
+  images: [
+    "1752147809896.jpg",
+    "1752147809090.jpg",
+    "1752147809675.jpg",
+    "1752147807270.mp4"
+  ],
+  colors: ["Black"],
+  description: `
+<ul>
+  <li><strong>Product Code: DP684200119AZASZE</strong></li>
+  <li><strong>Product Description</strong>: Discover the ultimate Smart Watch designed for health enthusiasts! Featuring Bluetooth 5.3, customizable watch faces, and a long-lasting battery, this sleek device is compatible with all Android and iPhone devices. Enjoy heart rate monitoring, GPS tracking, and voice control for effortless daily tasks. Elevate your fitness journey today with this stylish and durable smartwatch!</li>
+  <li>*Product Details*:Material: ABS Plastic</li>
+  <li>Connectivity Tech: Wireless</li>
+  <li>Bluetooth Version: Bluetooth 5.3</li>
+  <li>Product Feature: Health And Fitness Tracking, Long Battery Life, Long-Lasting Battery With Fast Charge, Durable Protection Against Water And Dust, Seamless Connectivity Across Devices, Sleek And Stylish Design, Compatible With All Android Devices, Compatible With iPhone Devices, Heart Rate Monitoring, Gps, Notifications, Nfc For Payments, And Customizable Watch Faces, Voice Control Features For Tasks Like Setting Reminders, Sending Messages, Or Controlling Smart Home Devices., 7 In 1</li>
+  <li>Color: Black</li>
+  <li>Package Includes: 1 x Smart Watch, 1 x Charger, 1 x Box, 1 x Manual</li>
+  <li>1 Model Number</li>
+  <li>Battery Capacity: 1000 mAh</li>
+  <li>Standby Time: 100 Hours</li>
+  <li>Voltage: 5 Volts</li>
+  <li>Watts: 2 Watt</li>
+  <li>Weight: 65 Gram</li>
+  <li>Length: 3 Inches</li>
+  <li>Width: 2 Inches</li>
+  <li>Height: 0.5 Inches</li>
+  <li>Display: Large, high-definition screen for easy navigation and viewing. 10-in-1 Package: Includes multiple interchangeable straps, a protective case, and wireless earphones. Fitness Tracking: Equipped with a heart rate monitor, blood oxygen sensor, sleep tracker, and step counter. Connectivity: Seamless integration with Android and iOS devices for calls, messages, and social media notifications. Additional Features: Remote camera control, music playback, and GPS tracking. Durability: Water-resistant design with a protective case for everyday wear. Battery Life: Long-lasting battery that supports days of usage on a single charge.</li>
+  <li>Note: Please ensure to follow the instructions provided in the user manual for proper usage and safety precautions.</li>
+</ul>
+`
+},
+{
+  title: "MDF Wall Clock & Wall Hanging",
+  price: "Rs.570",
+  image: "1752148116538.jpg",
+  images: [
+    "1752148116538.jpg",
+    "1752148117564.jpg",
+    "1752148118350.jpg"
+  ],
+  colors: [],
+  description: `
+<ul>
+  <li><strong>Product Code: DP24800253HDHDC</strong></li>
+  <li><strong>Product Description</strong>: Attractive Wall Clock Designs For Homes And Office</li>
+  <li>*Product Details*:Material: MDF</li>
+  <li>Product Type: Wall Clock</li>
+  <li>Shape: Leaf & Ahad</li>
+  <li>Size: 25 x 25 Inches</li>
+  <li>Thickness: 3mm</li>
+  <li>Package Includes: 1 x Wall Clock, 1 x Wall Hanging</li>
+  <li>Product Details: Laser Cut, Analogue, Complete Clock With Double Tape To Hang. Make Your Home Beautiful. This Product Enhances Any Home With On-trend Style And Beautiful Eye-catching Modern Design</li>
+  <li>Disclaimer: Product color may slightly vary due to photographic lighting sources or your monitor/screen settings.</li>
+</ul>
+`
+},
 ];
