bdff7b3df3bbad2e4133171c413ffdfe1fb9b500
Daniyal20251 <araindaniyalo2@gmail.com>
2025-08-20 08:20:06 +0500

    Add files via upload

--- itemData.js
+++ itemData.js
@@ -1,7 +1,7 @@
 // ✅ Global values for all cards
 const globalProfit = 100;    // profit
 const globalDiscount = 80;  // discount
-const DELIVERY_CHARGES = 0;
+const DELIVERY_CHARGES = 199;
 
 const items = [
  {
