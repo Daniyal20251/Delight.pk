ff7733e26b7d815dc1dd5f66646d2684f44ea884
Daniyal20251 <araindaniyalo2@gmail.com>
2025-09-06 10:11:15 +0500

    Add files via upload

--- sale.js
+++ sale.js
@@ -1,69 +1,10 @@
 // sale.js
 const flashSaleProducts = [
-  {
-  title: "2 Pcs Men's Dri Fit Printed Track Suit",
-  price: 1060,
-  finalPrice: 763,
-  discountPercentage: 28 ,
-  image: "1754395089331.jpg",
-  images: [
-    "1754395089331.jpg",
-    "1754395089692.jpg",
-    "1754395090175.jpg"
-  ],
-  colors: ["Small", "Medium", "Large", "X-Large", "2X-Large"],
-  description: `
-<ul>
-  <li><strong>Product Code: DP34920012SATR</strong></li>
-  <li><strong>Product Description</strong>: </li>
-  <li>*Product Details*:Gender: Men's</li>
-  <li>Fabric: Dri Fit</li>
-  <li>Pattern: Printed</li>
-  <li>Neck Type: Round Neck</li>
-  <li>Sizes: Small, Medium, Large, X-Large, 2X-Large</li>
-  <li>Number Of Pieces: 2 Pcs</li>
-  <li>Package Includes: 1 x Track Suit</li>
-  <li>Note: There might be an error of 1-3 cm due to manual measurement, and slight color differences may occur as a result of varying lighting and monitor effects.</li>
-</ul>
-`,
-stock: 1,
-sold: 0
-},
- {
-  title: "Rechargeable Table Lamp",
-  price: 1549,
-  finalPrice: 1254,
-  discountPercentage: 19 ,
-  image: "1754504330808.jpg",
-  images: [
-    "1754504330808.jpg",
-    "1754504331178.jpg",
-    "1754504331544.jpg",
-    "1754504331946.jpg",
-    "1754504332434.jpg"
-  ],
-  colors: ["Multicolor"],
-  description: `
-<ul>
-  <li><strong>Product Code: DP1518200291BGSE</strong></li>
-  <li><strong>Product Description</strong>: </li>
-  <li>*Product Details*:Material: ABS Plastic</li>
-  <li>Connectivity Tech: USB</li>
-  <li>Product Feature: Rechargeable</li>
-  <li>Light Color: Golden, Red, Green, Blue, White</li>
-  <li>Color: Multicolor</li>
-  <li>Package Includes: 1 x Table Lamp</li>
-  <li>Note: There might be an error of 1-3 cm due to manual measurement, and slight color differences may occur as a result of varying lighting and monitor effects.</li>
-</ul>
-`,
-stock: 1,
-sold: 0
-},
-  {
+   {
   title: "Black Arabic Aura Watch",
   price: 1489,
-  finalPrice: 1191 ,
-  discountPercentage: 20,
+  finalPrice: 967 ,
+  discountPercentage: 35,
   image: "1754394569129.jpg",
   images: [
     "1754394569129.jpg",
@@ -119,6 +60,65 @@ sold: 0
   <li>Rich, high-quality audio and voice</li>
   <li>24 Hour Battery Life</li>
   <li>Note: Please ensure to follow the instructions provided in the user manual for proper usage and safety precautions.</li>
+</ul>
+`,
+stock: 1,
+sold: 0
+},
+  {
+  title: "2 Pcs Men's Dri Fit Printed Track Suit",
+  price: 1060,
+  finalPrice: 763,
+  discountPercentage: 28 ,
+  image: "1754395089331.jpg",
+  images: [
+    "1754395089331.jpg",
+    "1754395089692.jpg",
+    "1754395090175.jpg"
+  ],
+  colors: ["Small", "Medium", "Large", "X-Large", "2X-Large"],
+  description: `
+<ul>
+  <li><strong>Product Code: DP34920012SATR</strong></li>
+  <li><strong>Product Description</strong>: </li>
+  <li>*Product Details*:Gender: Men's</li>
+  <li>Fabric: Dri Fit</li>
+  <li>Pattern: Printed</li>
+  <li>Neck Type: Round Neck</li>
+  <li>Sizes: Small, Medium, Large, X-Large, 2X-Large</li>
+  <li>Number Of Pieces: 2 Pcs</li>
+  <li>Package Includes: 1 x Track Suit</li>
+  <li>Note: There might be an error of 1-3 cm due to manual measurement, and slight color differences may occur as a result of varying lighting and monitor effects.</li>
+</ul>
+`,
+stock: 1,
+sold: 0
+},
+ {
+  title: "Rechargeable Table Lamp",
+  price: 1549,
+  finalPrice: 1254,
+  discountPercentage: 19 ,
+  image: "1754504330808.jpg",
+  images: [
+    "1754504330808.jpg",
+    "1754504331178.jpg",
+    "1754504331544.jpg",
+    "1754504331946.jpg",
+    "1754504332434.jpg"
+  ],
+  colors: ["Multicolor"],
+  description: `
+<ul>
+  <li><strong>Product Code: DP1518200291BGSE</strong></li>
+  <li><strong>Product Description</strong>: </li>
+  <li>*Product Details*:Material: ABS Plastic</li>
+  <li>Connectivity Tech: USB</li>
+  <li>Product Feature: Rechargeable</li>
+  <li>Light Color: Golden, Red, Green, Blue, White</li>
+  <li>Color: Multicolor</li>
+  <li>Package Includes: 1 x Table Lamp</li>
+  <li>Note: There might be an error of 1-3 cm due to manual measurement, and slight color differences may occur as a result of varying lighting and monitor effects.</li>
 </ul>
 `,
 stock: 1,
