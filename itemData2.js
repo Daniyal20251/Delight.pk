54281929efb19248cbbcf63f1407a39b003245d9
Daniyal20251 <araindaniyalo2@gmail.com>
2025-10-07 23:25:31 +0500

    Add files via upload

--- itemData2.js
+++ itemData2.js
@@ -1,6 +1,6 @@
 const storeHeading = "Dynamix"; 
-const globalProfit2 = 0;    // profit
-const globalDiscount2 = 0;  // discount
+const globalProfit2 = 500;    // profit
+const globalDiscount2 = 150;  // discount
 const DELIVERY_CHARGES2 = 250;
 
 const items2= [
@@ -19,7 +19,6 @@ const items2= [
   colors: [],
   description: `
 <ul>
-  <li>*Product Name*: 7 In 1 Gadgets Cleaning Kit</li>
   <li>*Product Description*: Soft Brush Does Not Hurt The Keyboard</li>
   <li>*Product Details*:Material: Abs Plastic</li>
   <li>Product Features: Multifunctional Cleaning Set: Flocking Sponge Can Clean The Dust In The Wireless Charging Box Of The Headset. The High-Density Brush Can Clean The Dirt On The Sound Outlet Of The Headset. The Nib Can Clean Stubborn Dust Flocking Sponges, High-Density Brushes, Pen Tips And Keycap Pullers.</li>
@@ -27,6 +26,61 @@ const items2= [
   <li>Note: Please ensure to follow the instructions provided in the user manual for proper usage and safety precautions.</li>
   <li></li>
   <li>Product Code: DP49200109AFTRD</li>
+</ul>
+`
+},
+{
+  title: "Fast Charging Keychain Power Bank",
+  price: "Rs.1289",
+  image: "IMG-20251007-WA0083.jpg",
+  images: [
+    "IMG-20251007-WA0083.jpg",
+    "IMG-20251007-WA0082.jpg",
+    "IMG-20251007-WA0084.jpg",
+    "IMG-20251007-WA0085.jpg"
+  ],
+  colors: ["Black"],
+  description: `
+<ul>
+  <li>*Product Description*: *Product Details*:Material: ABS Plastic Charging Ports: 1 USB C Product Feature: Fast Charging Color: Black Package Includes: 1 x Power Bank Note: Please ensure to follow the instructions provided in the user manual for proper usage and safety precautions. Product Code: DP94300000547PMGTIM</li>
+</ul>
+`
+},
+{
+  title: "3 Pcs Cotton Salonica Printed Double Bedsheet",
+  price: "Rs.2499",
+  image: "IMG-20251007-WA0087.jpg",
+  images: [
+    "IMG-20251007-WA0087.jpg"
+  ],
+  colors: [],
+  description: `
+<ul>
+  <li>*Product Details*:Fabric: Cotton Salonica Pattern: Printed Bed Size: Double Bed Number Of Pieces: 3 Pcs Package Includes: 1 x Bedsheet, 2 x Pillow Covers Bedsheet Length: 92 Inches Bedsheet Width: 89 Inches Pillow Length: 29 Inches Pillow Width: 19 Inches Color: Pink Note: There might be an error of 1-3 cm due to manual measurement, and slight color differences may occur as a result of varying lighting and monitor effects. Product Code: DP11249216062ADTE</li>
+</ul>
+`
+},
+{
+  title: "Men's PU Leather Wallet",
+  price: "Rs.1289",
+  image: "IMG-20251007-WA0088.jpg",
+  images: [
+    "IMG-20251007-WA0088.jpg",
+    "IMG-20251007-WA0041.jpg",
+    "IMG-20251007-WA0044.jpg",
+    "IMG-20251007-WA0040.jpg",
+    "IMG-20251007-WA0042.jpg",
+    "IMG-20251007-WA0043.jpg",
+    "IMG-20251007-WA0045.jpg",
+    "IMG-20251007-WA0046.jpg",
+    "VID-20251007-WA0047.mp4"
+  ],
+  colors: [],
+  
+  description: `
+<ul>
+  <li>*Product Details*:
+  Material: PU Leather Pattern: Plain Gender:Men's Product Feature: Bi-Fold Wallet, Pocket Friendly Size, Easy To Carry, Stylish And Slim Number Of Pieces: 1 Pc Color: Multicolor Note: There might be slight color differences as a result of varying lighting and monitor effects. Product Code: DP1772200007FFSE</li>
 </ul>
 `
 },
